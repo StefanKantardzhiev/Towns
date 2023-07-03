297833532d0f091bbfd7964db00eac95e00163a6
stefan-softuni-styler2 <ivanowa88@gmail.com>
2023-07-03 16:16:01 +0200

    styling

--- towns.js
+++ towns.js
@@ -19,9 +19,16 @@ function deleteTown() {
 		}
 	}
 	if (removed)
-		$('#result').text(townName + " deleted.");
+		showMessage(townName + " deleted.");
 	else
-		$('#result').text(townName + " not found.");
+		showMessage(townName + " not found.");
+}
+
+function showMessage(msg) {
+	$('#result').text(msg).css("display", "block");
+	setTimeout(function () {
+		$('#result').hide('blind', {}, 500);
+	}, 3000);
 }
 
 
@@ -46,26 +53,3 @@ function addTown() {
 	$('#towns').append($('<option>').text(townName));
 	$('#result').text(townName + " added.");
 }
-
-function deleteTown() {
-	let townName = $('#townName').val();
-	$('#townName').val('');
-	let removed = false;
-	for (let option of $('#towns option')) {
-	if (option.textContent == townName) {
-	removed = true;
-	option.remove();
-	}
-	}
-	if (removed)
-	showMessage(townName + " deleted.");
-	else
-	showMessage(townName + " not found.");
-   }
-   function showMessage(msg) {
-	$('#result').text(msg).css("display", "block");
-	setTimeout(function () {
-	$('#result').hide('blind', {}, 500);
-	}, 3000);
-   }
-   
